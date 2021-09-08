a5748c59890102f5feee3e9a7d12c00b730a8d2e
anonymerd <rhtbisht738@gmail.com>
2021-09-09 01:52:30 +0530

    Updated Basic Commands

---
+++ CommandHandlers/basicHandlers.js
@@ -0,0 +1,38 @@
+const pingHandler = (client, message, messageToBot) => {
+  message.reply('Pong!');
+};
+
+const hiHandler = (client, message, messageToBot) => {
+  message.reply('Hi!');
+};
+
+const avatarHandler = (client, message, messageToBot) => {
+  message.reply(message.author.displayAvatarURL());
+};
+
+const testHandler = (client, message, messageToBot) => {
+  message.reply('Working Bitch!');
+};
+
+export default [
+  {
+    name: 'ping',
+    description: 'Replies with Pong!',
+    handler: pingHandler,
+  },
+  {
+    name: 'hello',
+    description: 'Replies with Hi!',
+    handler: hiHandler,
+  },
+  {
+    name: 'avatar',
+    description: "Replies with the sender's display avatar!",
+    handler: avatarHandler,
+  },
+  {
+    name: 'test',
+    description: 'Replies accordingly!',
+    handler: testHandler,
+  },
+];
